4a4a1f7d650cf24b5910a469ef131606887b2ca9
Michael <micha.samson@live.ca>
2021-10-05 09:49:51 -0400

    Change ga

--- problematique/scripts/optimize-ga/main.py
+++ problematique/scripts/optimize-ga/main.py
@@ -23,10 +23,10 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
-# Author: Simon Brodeur <simon.brodeur@usherbrooke.ca>
-# Université de Sherbrooke, APP3 S8GIA, A2018
-
+# Author: Xavier Groleau <xavier.groleau@@usherbrooke.ca>
+# Author: Charles Quesnel <charles.quesnel@@usherbrooke.ca>
+# Author: Michael Samson <michael.samson@@usherbrooke.ca>
+# Université de Sherbrooke, APP2 S8GIA, A2018
 import os
 import sys
 import time
@@ -51,9 +51,7 @@ def initPopulation(env, popSize):
     population = []
     for i in range(popSize):
         population.append(env.action_space.sample())
-    print(population[0])
-    return population
-
+    return reorderGears(population)
 def breedNewGeneration(population, fitness, popSize, crossoverProb, mutationProb):
     newPopulation = []
     numPairs = int(popSize / 2)
@@ -64,16 +62,15 @@ def breedNewGeneration(population, fitness, popSize, crossoverProb, mutationProb
         child1, child2 = doCrossover(genitor1, genitor2, crossoverProb)
         newPopulation.extend([child1, child2])
     newPopulation = np.array(newPopulation)
-
+    newPopulation = doMutation(newPopulation, mutationProb)
     # Apply mutation to all individuals in the population
-    return doMutation(newPopulation, mutationProb)
+    return reorderGears(newPopulation)
     #return newPopulation
     
 def doSelection(population, fitness, numPairs):
     # Compute selection probability distribution
     eps = 1e-16
     fitness = fitness - np.min(fitness) + eps
-
     selectProb = np.cumsum(fitness) / np.sum(fitness)
     # Perform a roulette-wheel selection
     pairs = []
@@ -90,16 +87,17 @@ def doCrossover(genitor1, genitor2, crossoverProb):
     if crossoverProb > np.random.uniform():
         dataGen1 = list(genitor1.items())
         dataGen2 = list(genitor2.items())
+        # Select a random crossover point and create 2 child
         idx = np.random.randint(low=0, high=len(dataGen1))
         arrayChild1 = dataGen1[:idx] + dataGen2[idx:]
         arrayChild2 = dataGen2[:idx] + dataGen1[idx:]
         child1 = dict(arrayChild1)
         child2 = dict(arrayChild2)
         
-    return child1, child1
-
+    return child1, child2
 def doBinaryMutation(element, mutationProb):
     element = encodeElement(element)
+    # Mutate a certain percentage of bits
     mutatedBits = np.random.uniform(size=element.shape) < mutationProb
     nelement = np.logical_xor(element.astype(np.bool), mutatedBits).astype(element.dtype)
     newElement = decodeElement(nelement)
@@ -107,7 +105,8 @@ def doBinaryMutation(element, mutationProb):
 
 def encodeElement(element):
     bvalues = []
-    for key in element:
+    # Encode each value of the different parameters
+    for key in getKeys():
         minimum, rangeOfValue = getRange(key)
         binValue = ufloat2bin((element[key] - minimum) / rangeOfValue , getNbits(key))
         bvalues.extend(binValue[0])
@@ -115,6 +114,7 @@ def encodeElement(element):
 
 def decodeElement(element):
     newValue = {}
+    # Recreate the dict representing the parameters 
     for i in range(8): 
         key = getKeyForI(i)
         nbits = getNbits(key)
@@ -124,7 +124,6 @@ def decodeElement(element):
         newValue[key] = (bin2ufloat(value, nbits) * rangeOfValue) + minimum
         
     return newValue
-
 def ufloat2bin(cvalue, nbits):
     if nbits > 64:
         raise Exception('Maximum number of bits limited to 64')
@@ -141,14 +140,13 @@ def ufloat2bin(cvalue, nbits):
     bvalue[np.logical_and(ivalue >= 0, ivalue <= 2**nbits - 1)] = (np.bitwise_and(np.tile(ivalue[:, np.newaxis], (1, nbits)), np.tile(bitmask[np.newaxis, :], (len(cvalue), 1))) != 0)
     return bvalue
 
-
 def bin2ufloat(bvalue, nbits):
     if nbits > 64:
         raise Exception('Maximum number of bits limited to 64')
     ivalue = np.sum(bvalue * (2**np.arange(nbits)[np.newaxis, :]), axis=-1)
     cvalue = ivalue / (2**nbits - 1)
-    return cvalue
 
+    return cvalue
 
 def doMutation(population, mutationProb):
     for i in range(len(population)):
@@ -156,11 +154,11 @@ def doMutation(population, mutationProb):
 #        for key in population[i]:    
 #            if mutationProb > np.random.uniform():
 #                population[i][key] = np.array([randomInRange(key)])
-    #mutatedBits = np.random.uniform(size=population.shape) < mutationProb
-    #npopulation = np.logical_xor(population.astype(np.bool), mutatedBits).astype(population.dtype)
+                
     return population
 
 def getNbits(key):
+    # Return the # of bits wanted for each key
     if 'gear' in key: 
         return 7
     elif 'spoiler' in key:
@@ -168,7 +166,12 @@ def getNbits(key):
     else:
         return 6
     
+def getKeys():
+    return ['front-spoiler-angle', 'gear-2-ratio', 'gear-3-ratio', 
+            'gear-4-ratio', 'gear-5-ratio', 'gear-6-ratio', 
+            'rear-differential-ratio', 'rear-spoiler-angle']
 def getKeyForI(i):
+    # Return the key value for a certain position in list
     if i == 0: 
         return 'front-spoiler-angle'
     elif i == 6:
@@ -179,6 +182,7 @@ def getKeyForI(i):
         return 'gear-' + str(i+1) + '-ratio'
         
 def randomInRange(key):
+    # Gives a random value for a key
     if 'gear' in key: 
         return np.random.uniform(low=0.1, high=5.0)
     elif 'spoiler' in key:
@@ -187,6 +191,7 @@ def randomInRange(key):
         return np.random.uniform(low=1.0, high=10.0)
     
 def getRange(key):
+    # Returns the minimum and the range for a certain key
     if 'gear' in key: 
         return (0.1, 5.0 - 0.1)
     elif 'spoiler' in key:
@@ -195,20 +200,33 @@ def getRange(key):
         return (1.0, 10.0 - 1.0)
     
 def getAverage(observations, key):
+    # Returns the average value of the key observations
     numElement = len(observations)
     average = 0.0
     for observation in observations:
         average += observation[key][0] / numElement
     return average
-
-def showFigure(numIteration, maxRecord, overallMaxRecord, avgRecord, valueShown):
+def reorderGears(newPopulation):
+    for i in range(len(newPopulation)):
+        param, data = list(zip(*list(newPopulation[i].items())))
+        indexEnd= param.index('gear-6-ratio') + 1
+        indexStart = param.index('gear-2-ratio')
+        sortedGears = list(data)[:indexStart] + sorted(list(data)[indexStart:indexEnd], key=lambda value: value[0], reverse=True) + list(data)[indexEnd:]
+        newPopulation[i] = dict(zip(param, sortedGears))
+        
+    return newPopulation
+    
+def showFigure(numIteration, maxRecord, overallMaxRecord, avgRecord, valueShown, breedingRate, mutationRate, popSize):
     fig = plt.figure()
     n = np.arange(numIteration)
     ax = fig.add_subplot(111)
     ax.plot(n, maxRecord, '-r', label='For Generation Max')
     ax.plot(n, overallMaxRecord, '-b', label='For Overall Max')
     ax.plot(n, avgRecord, '--k', label='Generation Average')
-    ax.set_title(valueShown + ' value over generations')
+    ax.set_title(valueShown + ' value over generations (br: ' + 
+                 str(breedingRate) + ', mr: ' + str(mutationRate) + 
+                 ') on a population of ' + str(popSize) + ' for ' + 
+                 str(numIteration) + ' generations')
     ax.set_xlabel('Generation')
     ax.set_ylabel(valueShown + ' value')
     ax.legend()
@@ -216,7 +234,7 @@ def showFigure(numIteration, maxRecord, overallMaxRecord, avgRecord, valueShown)
 
     plt.show()
             
-def main(maxEvaluationTime=40, optimizeFor='distRaced', breedingRate=0.8, mutationRate=0.01, popSize=50, numIteration=150):
+def main(maxEvaluationTime=40, optimizeFor='fuelUsed', breedingRate=0.7, mutationRate=0.01, popSize=100, numIteration=100):
     try:
         with TorcsOptimizationEnv(maxEvaluationTime) as env:
             population = initPopulation(env, popSize)
@@ -241,7 +259,7 @@ def main(maxEvaluationTime=40, optimizeFor='distRaced', breedingRate=0.8, mutati
                 observations = []
                 for parameters in population:
                     # Generate a random vector of parameters in the proper interval
-                    # logger.info('Generated new parameter vector: ' + str(parameters))
+#                    logger.info('Generated new parameter vector: ' + str(parameters))
     
                     # Perform the evaluation with the simulator
                     observation, _, _, _ = env.step(parameters)
@@ -252,8 +270,9 @@ def main(maxEvaluationTime=40, optimizeFor='distRaced', breedingRate=0.8, mutati
                     else:
                         fitness.append(observation[optimizeFor][0])
                     observations.append(observation)
-
-
+                    
+ 
+                    
                 maxIndex = fitness.index(max(fitness))
                 if fitness[maxIndex] > bestIndividualFitness:
                     bestIndividual = population[maxIndex]
@@ -272,28 +291,29 @@ def main(maxEvaluationTime=40, optimizeFor='distRaced', breedingRate=0.8, mutati
                 maxSpeedRecord[i] = observations[maxIndex]['topspeed'][0]
                 overallMaxSpeedRecord[i] = bestIndividualObservations['topspeed'][0]
                 avgMaxSpeedRecord[i] = getAverage(observations, 'topspeed')
-                logger.info('Best %s   =   %f', optimizeFor, fitness[maxIndex])
-                
+#                logger.info('Best %s   =   %f', optimizeFor, fitness[maxIndex])
+#                logger.info('Generated new parameter vector: ' + str(population[maxIndex]))
                 # Get new Gen
                 population = breedNewGeneration(population, fitness, popSize, breedingRate, mutationRate)
                 
-            showFigure(numIteration, maxFitnessRecord, overallMaxFitnessRecord, avgMaxFitnessRecord, 'Fitness')
-            showFigure(numIteration, maxFuelRecord, overallMaxFuelRecord, avgMaxFuelRecord, 'Fuel')
-            showFigure(numIteration, maxDistanceRecord, overallMaxDistanceRecord, avgMaxDistanceRecord, 'Distance')
-            showFigure(numIteration, maxSpeedRecord, overallMaxSpeedRecord, avgMaxSpeedRecord, 'Top speed')
-
-           
+#            showFigure(numIteration, maxFitnessRecord, overallMaxFitnessRecord, avgMaxFitnessRecord, 'Fitness', breedingRate, mutationRate, popSize)
+#            showFigure(numIteration, maxFuelRecord, overallMaxFuelRecord, avgMaxFuelRecord, 'Fuel',  breedingRate, mutationRate, popSize)
+#            showFigure(numIteration, maxDistanceRecord, overallMaxDistanceRecord, avgMaxDistanceRecord, 'Distance',  breedingRate, mutationRate, popSize)
+#            showFigure(numIteration, maxSpeedRecord, overallMaxSpeedRecord, avgMaxSpeedRecord, 'Top speed',  breedingRate, mutationRate, popSize)
+            
 #                 Display simulation results
             logger.info('##################################################')
             logger.info('Results:')
             logger.info('Time elapsed (sec) =   %f', maxEvaluationTime)
+            logger.info('Parameters (br, mr, pop, #gen)=   %f %f %d %d', breedingRate, mutationRate, popSize, numIteration)
             logger.info('Top fitness        =   %f', overallMaxFitnessRecord[-1])
             logger.info('Top speed (km/h)   =   %f', overallMaxSpeedRecord[-1])
             logger.info('Distance raced (m) =   %f', overallMaxDistanceRecord[-1])
             logger.info('Fuel used (l)      =   %f', overallMaxFuelRecord[-1])
             logger.info(bestIndividual)
             logger.info('##################################################')
-
+            return maxFitnessRecord
+            
     except TorcsException as e:
         logger.error('Error occured communicating with TORCS server: ' + str(e))
 
@@ -301,8 +321,29 @@ def main(maxEvaluationTime=40, optimizeFor='distRaced', breedingRate=0.8, mutati
         pass
 
     logger.info('All done.')
-
-
+    
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    main()
+    baseConfig = [0.1, 0.01]
+    stepConfig = [0.1, 0.01]
+    result = []
+    generalBest = ([], 0, 0, 0)
+    fig = plt.figure()
+#    main()
+    for j in range(2):
+        resultI = []
+        for i in range(5):
+            config = baseConfig.copy()
+            config[0] = baseConfig[0] + stepConfig[0] * i             
+            config[1] = baseConfig[1] + stepConfig[1] * j 
+            bestResult = main(40, 'distRaced', config[0], config[1], 50, 100)
+            plt.plot(bestResult, label=('br: ' + str(config[0]) + ' mr: ' + str(config[1])))
+#            candidate, candidateFitness = bestResult
+#            _, currentBestFitness, _, _ = generalBest
+#            resultI.append((candidate, candidateFitness, config[0], config[1]))
+#            if (candidateFitness > currentBestFitness):
+#                generalBest = (candidate, candidateFitness, config[0], config[1])
+        result.append(resultI)
+    plt.legend()
+    plt.show()                
+    
